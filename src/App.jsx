bc8c6a6cbd9a78edbff5a94cdffae3d15bcc6566
maciejSamerdak <sam.maciej@interia.pl>
2025-02-17 18:04:14 +0100

    Create main page and forward request to GitHub API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './assets/styles/App.css'
+import { useState } from "react"
+import SearchBar from "@/components/SearchBar"
+import RepositoryList from "@/components/RepositoryList"
+import { Spinner, Container } from "react-bootstrap"
+import axios from "axios"
+import "@/assets/styles/App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isSearching, setIsSearching] = useState(false)
+    const [isError, setIsError] = useState(false)
+    const [repositoryList, setRepositoryList] = useState(null)
+    const [errorMessage, setErrorMessage] = useState("")
+    const env = import.meta.env
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const getRequestParams = username => {
+        const url = `${env.VITE_GITHUB_API_URL}/users/${username}/repos`
+        let headers = {
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": env.VITE_GITHUB_API_VERSION
+        }
+        if (env.VITE_GITHUB_API_KEY) {
+            headers["Authorization"] = `Bearer ${env.VITE_GITHUB_API_KEY}`
+        }
+        return [url, { headers }]
+    }
+
+    const handleSubmit = username => {
+        setIsError(false)
+        setIsSearching(true)
+
+        const [url, requestBody] = getRequestParams(username)
+        axios.get(url, requestBody).then(response => {
+            setRepositoryList(response.data)
+        }).catch(error => {
+            if (error.status === 404) {
+                setErrorMessage(`No matches found for "${username}"`)
+            } else if (error.status === 403) {
+                setErrorMessage("API rate limit has been exceeded")
+            } else {
+                setErrorMessage("An error has occured while processing your request")
+            }
+            setIsError(true)
+        }).finally(() => (
+            setIsSearching(false)
+        ))
+    }
+
+    let content
+    if (isError) {
+        content = <div className="content-placeholder"><h6>{errorMessage}</h6></div>
+    } else if (isSearching) {
+        content = <div className="content-placeholder"><Spinner /></div>
+    } else if (repositoryList !== null) {
+        content = <RepositoryList repositories={repositoryList} />
+    }
+
+    return (
+        <Container>
+            <div id="page-header">
+            <h1>GitHub Repository Browser</h1>
+            <h3>{"Type in a GitHub user's name and see all repositories they own!"}</h3>
+            </div>
+            <div id="page-content">
+            <div id="search-bar-wrapper">
+                <SearchBar busy={isSearching} onSubmit={handleSubmit}/>
+            </div>
+            { content }
+            </div>
+        </Container>
+    )
 }
 
 export default App
